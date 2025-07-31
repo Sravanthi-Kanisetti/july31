d26bd8e4dd90be9ff4127fecb9f09b3364fce3c5
Sravanthi-Kanisetti <kanisettisravanthi@gmail.com>
2025-07-31 18:23:07 +0530

    Update linearsearch.java

--- linearsearch.java
+++ linearsearch.java
@@ -1,6 +1,19 @@
-public class linearsearch{
-  public static void main(String [] args){
-  System.out.println("hellooooooooo Sravanthi ");
-  }
 
+public class linearsearch
+{
+	public static void main(String[] args) {
+		int[] nums={11,22,33,44,55,66,77,88,99};
+		int target=55;
+		int result=linear(nums,target);
+		System.out.println("the result is at index "+result);
+	}
+	
+	static int linear(int[] nums,int target){
+	    for(int i=0;i<nums.length;i++){
+	        if(nums[i]==target){
+	            return i;
+	        }
+	    }
+	    return -1;
+	}
 }
